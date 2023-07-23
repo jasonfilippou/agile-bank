3c31199f97488bcb6be1422ed491439417da0f8d
Jason Filippou <jason.filippou@gmail.com>
2023-07-23 19:33:31 +0300

    Disabled updates on createdBy and createdDate fields of Auditable class.

--- src/main/java/com/agilebank/model/Auditable.java
+++ src/main/java/com/agilebank/model/Auditable.java
@@ -15,18 +15,18 @@ import org.springframework.format.annotation.DateTimeFormat;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable<U> {
 
-    @Column(name = "created_by")
+    @Column(name = "created_by", updatable = false)
     @CreatedBy
     protected U createdBy;
+
+    @Column(name = "created_at", updatable = false)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @CreatedDate
+    protected Date createdDate;
 
     @Column(name = "last_modified_by")
     @LastModifiedBy
     protected U lastModifiedBy;
-
-    @Column(name = "created_at")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @CreatedDate
-    protected Date createdDate;
 
     @Column(name = "last_modified_date")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
